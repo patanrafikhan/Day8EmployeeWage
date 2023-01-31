ea567e05a1b61280526691083c90f18c988deb1d
patanrafikhan <121918502+patanrafikhan@users.noreply.github.com>
2023-01-31 22:48:49 +0530

    UC7 Employee Wage Computation

---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace UC7EmployeeWage
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Logic obj = new Logic();
+            obj.Employee();
+
+        } 
+
+    }
+    public class Logic
+    {
+        public void Employee() 
+        {
+            Random ran = new Random();
+            int WagePer_Hour = 20;
+            int FullDay_Hour = 8;
+            int HalfDay_Hour = 4;
+            int workingDaysPerMonth = 20;
+            int totalWage = 0;
+            int noOfDaysPresent = 20;
+            int totalHours = 0;
+
+            for (int i = 0; i < workingDaysPerMonth && totalHours <= 100; i++)
+            {
+                int number = ran.Next(0, 3);
+
+                switch (number)
+                {
+
+                    case 0:
+                        totalHours += FullDay_Hour;
+                        totalWage = totalWage + (FullDay_Hour * WagePer_Hour);
+                        break;
+
+                    case 1:
+                        totalHours += HalfDay_Hour;
+                        totalWage += (HalfDay_Hour * WagePer_Hour);
+                        break;
+
+                    case 2:
+                        noOfDaysPresent--;
+                        totalWage += 0;
+                        break;
+                    default:
+                        break;
+                }
+            }
+            Console.WriteLine("Number of Hours Employee worked: " + totalHours);
+            Console.WriteLine("Number of Days Employee is Present: " + noOfDaysPresent + " Days");
+            Console.WriteLine("total wages for the month is: " + totalWage);
+            Console.ReadKey();
+        }
+    
+    }
+}
