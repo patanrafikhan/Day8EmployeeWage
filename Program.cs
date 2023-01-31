aeaeae32875ad36088e948d026a5b877157bfb35
patanrafikhan <121918502+patanrafikhan@users.noreply.github.com>
2023-01-31 22:00:47 +0530

    UC1 Employee Wage Computation

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace UC1EmployeeWage
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("WElcome to Employee App");
+            Console.WriteLine("Enter Employee name :");
+            string name = Console.ReadLine();
+
+            Random ran = new Random();
+            int n = ran.Next(0, 2);
+
+            if (n == 0)
+            {
+                Console.WriteLine("Empolee is Absent ");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Present");
+            }
+        }
+    
+    }
+}
